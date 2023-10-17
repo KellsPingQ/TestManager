ca2a902bb98d6fdc8782c83dff18d9e043c8db29
Kells_P <kellsping@gmail.com>
2023-10-17 15:05:01 +0800

    Update. Add "TestDll" to print more text.

--- Program.cs
+++ Program.cs
@@ -3,14 +3,21 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using TestDll;
 
 namespace TestManager
 {
     internal class Program
     {
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello TestManager");
+            string name = "Windows";
+
+            Console.WriteLine("Hello TestManager.");
+            HelloService.HelloDll();
+            HelloService.HelloName(name);
+
             Console.ReadLine();
         }
     }
